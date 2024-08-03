8b759a68ebd79dd4a1da4c085eabd1815acc9faf
Youssef Hamed <yousiefhamed@gmail.com>
2024-08-03 14:08:35 +0300

    setting the env & adding the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        sm: "400px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [],
