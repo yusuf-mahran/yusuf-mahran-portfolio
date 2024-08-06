3014da7f2c8f8e7f1451134a773508de287e8c9e
Youssef Hamed <yousiefhamed@gmail.com>
2024-08-06 07:52:14 +0300

    fixing grid issue in footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -8,7 +8,7 @@ const ruthie = Ruthie({ subsets: ["latin"], weight: "400" });
 export default function Footer() {
   return (
     <footer className="mt-12 bg-screen bg-gradient w-full md:px-12 px-5 pt-12 pb-2 grid md:grid-cols-12 grid-cols-1 gap-10">
-      <div className="w-full col-span-5 flex flex-col justify-center items-center text-center gap-5 max-w-80">
+      <div className="w-full md:col-span-5 flex flex-col justify-center items-center text-center gap-5 max-w-80">
         <h3 className={`${ruthie.className} text-6xl`}>Youssef Hamed</h3>
         <Line width="100%" />
         <Para color="color-light-gray">
@@ -16,11 +16,11 @@ export default function Footer() {
           make a better world
         </Para>
       </div>
-      <div className="w-full col-span-7">
+      <div className="w-full md:col-span-7">
         <Social color="color-light-gray" />
       </div>
       <div
-        className="col-span-12 text-center border-t-2
+        className="w-full md:col-span-12 text-center border-t-2
       py-2"
       >
         <Para color="color-light-gray">made with ❤️‍🔥 by youssef hamed 😉</Para>
