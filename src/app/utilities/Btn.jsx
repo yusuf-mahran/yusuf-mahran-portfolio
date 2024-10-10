39f297b6eaf75362d8518a4c0cec33724be5efb5
Youssef Hamed <yousiefhamed@gmail.com>
2024-10-10 09:38:47 +0300

    adding minWidth option to btn utils

--- src/app/utilities/Btn.jsx
+++ src/app/utilities/Btn.jsx
@@ -1,19 +1,32 @@
 import Link from "next/link";
 
-export default function Btn({ children, href, color, target, handleClick }) {
+export default function Btn({
+  children,
+  href,
+  color,
+  target,
+  handleClick,
+  minWidth,
+}) {
   return (
     <>
       {href ? (
         <Link
           href={href}
           target={target}
+          style={{
+            minWidth: minWidth ? minWidth : "",
+          }}
           className={`${color} btn flex justify-center items-center gap-3`}
         >
           {children}
         </Link>
       ) : (
         <button
-          style={{ background: color === "bg-transparent" && "transparent" }}
+          style={{
+            background: color === "bg-transparent" && "transparent",
+            minWidth: minWidth ? minWidth : "",
+          }}
           onClick={handleClick}
           className={`${color} btn flex justify-center items-center gap-3`}
         >
