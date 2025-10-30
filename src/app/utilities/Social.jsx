bd7b102316f31cda420089307b030dcaacf0775a
Yusuf Mahran <yusuf.hamed.dev@gmail.com>
2025-10-30 11:37:13 +0300

    Update contact information in Social component

--- src/app/utilities/Social.jsx
+++ src/app/utilities/Social.jsx
@@ -6,7 +6,7 @@ export default function Social({ color }) {
   return (
     <div className="flex flex-col justify-center items-start gap-4">
       <Link
-        href="https://wa.me/+201279237596"
+        href="https://wa.me/+201055601803"
         target="_blank"
         className="flex justify-start items-start gap-4 break-all"
       >
@@ -17,11 +17,11 @@ export default function Social({ color }) {
           height={30}
         />
         <Para color={color ? color : "color-primary-blue"}>
-          <span className="text-base">(+20)</span> <span>1279237596</span>
+          <span className="text-base">(+20)</span> <span>1055601803</span>
         </Para>
       </Link>
       <Link
-        href="mailto:yousiefhamed1@gmail.com"
+        href="mailto:yusuf.mahran.100@gmail.com"
         target="_blank"
         className="flex justify-start items-start gap-4 break-all"
       >
@@ -32,11 +32,11 @@ export default function Social({ color }) {
           height={30}
         />
         <Para color={color ? color : "color-primary-blue"}>
-          yousiefhamed1@gmail.com
+          yusuf.mahran.100@gmail.com
         </Para>
       </Link>
       <Link
-        href="https://www.linkedin.com/in/youssef-hamed-helmy/"
+        href="https://www.linkedin.com/in/yusuf-mahran/"
         target="_blank"
         className="flex justify-start items-start gap-4 break-all"
       >
@@ -47,11 +47,11 @@ export default function Social({ color }) {
           height={30}
         />
         <Para color={color ? color : "color-primary-blue"}>
-          https://linkedin.com/in/youssef-hamed-helmy/
+          https://linkedin.com/in/yusuf-mahran/
         </Para>
       </Link>
       <Link
-        href="https://github.com/yousiefhamed"
+        href="https://github.com/yusuf-mahran"
         target="_blank"
         className="flex justify-start items-start gap-4 break-all"
       >
@@ -62,7 +62,7 @@ export default function Social({ color }) {
           height={30}
         />
         <Para color={color ? color : "color-primary-blue"}>
-          https://github.com/yousiefhamed
+          https://github.com/yusuf-mahran
         </Para>
       </Link>
     </div>
